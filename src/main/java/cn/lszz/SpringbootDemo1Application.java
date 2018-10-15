a4f37f2b48a174e4d5a85eeadb413b7766853abb
Administrator <Administrator@PC-20180521TYDL>
2018-10-15 11:17:04 +0800

    remove jdbc,mysql and add a test

--- src/main/java/cn/lszz/SpringbootDemo1Application.java
+++ src/main/java/cn/lszz/SpringbootDemo1Application.java
@@ -2,11 +2,19 @@ package cn.lszz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootDemo1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDemo1Application.class, args);
+	}
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello world !!!";
 	}
 }
